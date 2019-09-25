e85acfece73d9b082500a2d075ed32303661168c
arch-123 <archanasgujar@gmail.com>
2019-09-25 11:12:11 +0530

    First Commit-Archana

---
+++ main.py
@@ -0,0 +1,12 @@
+
+if __name__== "__main__":
+	temp = True
+	while(temp):
+		input_1 = int(input("Integer 1:"))
+		input_2 = int(input("Integer 2:"))
+
+		print("1: Addition\n 2: Subtraction \n3: Multiply \n4: Divide")
+		process = int(input(select any:))
+
+
+		
